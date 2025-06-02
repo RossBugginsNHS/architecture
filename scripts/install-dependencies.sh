02869054ba3648c1dde41cf8034a4049f6c52944
Ross Buggins <ross.buggins@nhs.net>
2025-06-02 21:23:34 +0100

    npm install.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,8 +2,7 @@
 # This script installs the necessary dependencies for the project.
 
 bundle install || true
-npm install nunjucks --save
-npm install nhsuk-frontend --save
+npm install
 #wget https://github.com/nhsuk/nhsuk-frontend/releases/download/v9.6.1/nhsuk-frontend-9.6.1.zip -O nhsuk-frontend.zip
 #unzip -o -d ./assets/nhsuk nhsuk-frontend.zip
 #rm nhsuk-frontend.zip
